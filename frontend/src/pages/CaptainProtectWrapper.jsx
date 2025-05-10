474b35a53c0d51495e627adb856660b8c9fc1ebe
Jaanvi Nayak <nayakjaanvi321@gmail.com>
2025-05-10 10:02:53 +0530

    User UI completed

--- frontend/src/pages/CaptainProtectWrapper.jsx
+++ frontend/src/pages/CaptainProtectWrapper.jsx
@@ -1,15 +1,18 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { CaptainDataContext } from '../context/CaptainContext';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react'
+import { CaptainDataContext } from '../context/CaptainContext'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const CaptainProtectWrapper = ({
     children
 }) => {
+
     const token = localStorage.getItem('token')
     const navigate = useNavigate()
     const { captain, setCaptain } = useContext(CaptainDataContext)
     const [ isLoading, setIsLoading ] = useState(true)
+
+
 
     useEffect(() => {
         if (!token) {
@@ -22,22 +25,26 @@ const CaptainProtectWrapper = ({
             }
         }).then(response => {
             if (response.status === 200) {
-                setCaptain(response.data)
+                setCaptain(response.data.captain)
                 setIsLoading(false)
             }
         })
             .catch(err => {
-                console.log(err)
+
                 localStorage.removeItem('token')
                 navigate('/captain-login')
             })
     }, [ token ])
 
+    
+
     if (isLoading) {
         return (
-            <div> Loading...</div>
+            <div>Loading...</div>
         )
     }
+
+
 
     return (
         <>
